fd151a77035dfab72ccab368583ade1cf2f93a8a
dani-f99 <danielf3124@gmail.com>
2025-12-09 14:55:33 +0200

    add functionality to helper scripts, started motif_logo class

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,5 +3,5 @@ from .helpers import create_folders, read_json
 print("> Initiating required folders: ")
 create_folders()
 
-print("> Loading parameters from `congif.json` accessible as `congif` variable. ")
+print("> Loading parameters from `config.json` accessible as `config` variable. ")
 congif = read_json()
